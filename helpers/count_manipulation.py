37a5d17c7631952c31121c8f81959f6d9b6bfc78
hgoerner <hendrik.goerner@tu-dresden.de>
2022-08-01 16:22:40 +0200

    added guard clausel

--- helpers/count_manipulation.py
+++ helpers/count_manipulation.py
@@ -3,21 +3,23 @@ import helpers.objectstorage as objectstorage
 
 def assign_vehicle_class(event):
 
+    if not objectstorage.active_countings:
+        return
     active_count = objectstorage.active_countings[objectstorage.active_countings_index]
 
-    if event.keysym == "c" and objectstorage.active_countings:
+    if event.keysym == "c":
         print("assigning Class Car to count: " + str(active_count.ID))
 
         # change 0 to active_count_index
         active_count.Vhc_class = "Car"
 
-    if event.keysym == "b" and objectstorage.active_countings:
+    if event.keysym == "b":
         print("assigning Class Car to count: " + str(active_count.ID))
 
         # change 0 to active_count_index
         active_count.Vhc_class = "Bicycle"
 
-    if event.keysym == "t" and objectstorage.active_countings:
+    if event.keysym == "t":
         print("assigning Class Car to count: " + str(active_count.ID))
 
         # change 0 to active_count_index
