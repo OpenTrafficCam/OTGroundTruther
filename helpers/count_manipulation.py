c987fda5ee1d5c1c79d73038dd602cc98f469df9
hgoerner <hendrik.goerner@tu-dresden.de>
2022-08-01 15:54:43 +0200

    classes

--- helpers/count_manipulation.py
+++ helpers/count_manipulation.py
@@ -10,3 +10,15 @@ def assign_vehicle_class(event):
 
         # change 0 to active_count_index
         active_count.Vhc_class = "Car"
+
+    if event.keysym == "b" and objectstorage.active_countings:
+        print("assigning Class Car to count: " + str(active_count.ID))
+
+        # change 0 to active_count_index
+        active_count.Vhc_class = "Bicycle"
+
+    if event.keysym == "t" and objectstorage.active_countings:
+        print("assigning Class Car to count: " + str(active_count.ID))
+
+        # change 0 to active_count_index
+        active_count.Vhc_class = "Truck"
