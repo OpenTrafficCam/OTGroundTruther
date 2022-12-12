7763045df0291b28f744a42cbedfd66c4a653093
hgoerner <hendrik.goerner@tu-dresden.de>
2022-12-12 11:00:44 +0100

    update configfile

--- helpers/count/count_manipulation.py
+++ helpers/count/count_manipulation.py
@@ -1,21 +1,7 @@
 import helpers.filehelper.objectstorage as objectstorage
+from helpers.filehelper.config import vehicle_definition
 
-vehicle_definition = {
-    "1": "Pkw ohne Anhänger",
-    "3": "Fahrrad ohne Anhänger",
-    "2": "Person",
-    "4": "Bus",
-    "5": "Lkw ohne Anhänger",
-    "6": "Lkw mit Anhänger",
-    "7": "Pkw mit Anhänger",
-    "8": "Lieferwagen ohne Anhänger",
-    "9": "Lieferwagen mit Anhänger",
-    "equal": "Motorisierte Zweiräder",
-    "exclam":"Scooter",
-    "quotedbl": "Lastenrad",
-    "section": "Fahrrad mit Anhänger",
-    "percent":"Schienenfahrzeug"
-}
+
 
 
 def assign_vehicle_class(event):
--- helpers/filehelper/config.py
+++ helpers/filehelper/config.py
@@ -1,3 +1,17 @@
-#TODO: Migrate factors from objectstorage to config.py
-
-# store instance of classes here
+#definition of vehicleclasses and hotkeys as dictionary keys
+vehicle_definition = {
+    "1": "Pkw ohne Anhänger",
+    "3": "Fahrrad ohne Anhänger",
+    "2": "Person",
+    "4": "Bus",
+    "5": "Lkw ohne Anhänger",
+    "6": "Lkw mit Anhänger",
+    "7": "Pkw mit Anhänger",
+    "8": "Lieferwagen ohne Anhänger",
+    "9": "Lieferwagen mit Anhänger",
+    "equal": "Motorisierte Zweiräder",
+    "exclam":"Scooter",
+    "quotedbl": "Lastenrad",
+    "section": "Fahrrad mit Anhänger",
+    "percent":"Schienenfahrzeug"
+}
