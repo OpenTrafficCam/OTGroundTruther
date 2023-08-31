98c2b0e5cfe3cff654ef7f3dc70ba63448d05720
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2023-08-31 06:00:26 +0200

    Add help menu cascade

--- OTGroundTruther/gui/menu.py
+++ OTGroundTruther/gui/menu.py
@@ -13,9 +13,11 @@ class MenuBar(Menu):
 
     def _get_widgets(self) -> None:
         self._file_menu = FileMenu(master=self, tearoff=0, presenter=self._presenter)
+        self._help_menu = HelpMenu(master=self, tearoff=0, presenter=self._presenter)
 
     def _place_widgets(self) -> None:
         self.add_cascade(label="File", menu=self._file_menu)
+        self.add_cascade(label="Help", menu=self._help_menu)
 
 
 class FileMenu(Menu):
@@ -27,3 +29,13 @@ class FileMenu(Menu):
     def _get_and_place_widgets(self) -> None:
         self.add_command(label="Load videos", command=self._presenter.load_video_files)
         self.add_command(label="Load otflow", command=self._presenter.load_otflow)
+
+
+class HelpMenu(Menu):
+    def __init__(self, presenter: PresenterInterface, **kwargs: Any):
+        super().__init__(**kwargs)
+        self._presenter = presenter
+        self._get_and_place_widgets()
+
+    def _get_and_place_widgets(self) -> None:
+        pass
