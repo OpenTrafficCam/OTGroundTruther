cce45efc5edbcb9a1fc6830bb78f977fe733fe4f
martinbaerwolff <martin.baerwolff@tu-dresden.de>
2024-02-07 15:44:46 +0100

    Add script to run OTGroundTruther GUI

---
+++ OTGroundTruther.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+echo "Start OTGroundTruther."
+
+echo "$DIR"
+cd "$DIR" || exit
+
+source "$DIR"/venv/bin/activate
+python3.11 -m OTGroundTruther
+deactivate
